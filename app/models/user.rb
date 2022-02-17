2205ff93ab0779e8916c2089f95de8de45b0b962
Camilo Zamora <cazmera.music@gmail.com>
2022-02-17 20:27:26 +0100

    last changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :flats
-  has_many :booking_requests
+  has_many :booking_requests #Outgoing booking requests, booking as a customer
   has_many :messages
   has_many :chats, through: :flats
+  has_many :booking_as_owner, through: :flats, source: :booking_requests #Bookings incoming, to my flat
 end
