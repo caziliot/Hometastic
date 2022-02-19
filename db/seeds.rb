9103cc051a5cd0f6498292aea58883f8f391eb36
Joaquin Chumaceiro <cajoa@yahoo.com>
2022-02-19 12:08:43 +0100

    generated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Creating 5 booking_requests...'
+5.times do |i|
+  users = User.create!(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.name,
+    last_name: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+  puts "User ##{i}, #{users.first_name}"
+
+  # booking = BookingRequest.create!(
+  #   status: "Pending",
+  #   month_request: ,
+  #   price: ,
+  #   direction: ,
+  #   stay_status: ,
+  #   user_id: ,
+  #   flat_id:
+  # )
+end
+puts 'Finished!'
