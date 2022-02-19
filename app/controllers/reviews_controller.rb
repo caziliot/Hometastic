4db39e7244de4d18d327a47316da3554001f8c5d
Marcy Hundschell Dela Cruz <marcy.delacruz@gmail.com>
2022-02-19 16:58:49 +0100

    partially done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  def new
+    @booking = BookingRequest.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @booking = BookingRequest.find(params[:booking_id])
+    @review.booking = @booking
+    if @review.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
