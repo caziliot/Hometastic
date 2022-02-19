9acca51da9ab9c44aa16cde485bd252b910ccf96
Joaquin Chumaceiro <cajoa@yahoo.com>
2022-02-19 17:04:06 +0100

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ puts "start seeding"
     last_name: Faker::Name.last_name,
     phone_number: Faker::PhoneNumber.cell_phone,
     password: "123456"
+
   )
   puts "User ##{i + 1}, #{user.first_name}"
 
@@ -29,37 +30,39 @@ puts "start seeding"
     price: Faker::Commerce.price,
     description: Faker::Restaurant.description,
     city: Faker::Address.city,
-    availabity: ["Available", "Taken"].sample,
+    availabity: ["available", "taken"].sample,
     user_id: user.id
   )
   puts "flat ##{i + 1}, #{flat.address}"
+end
 
+5.times do |i|
   booking_request = BookingRequest.create!(
     status: ["available", "rented"].sample,
     month_request: Date::MONTHNAMES.sample,
     price: Faker::Commerce.price,
     direction: Faker::Address.street_address,
     stay_status: ["stay in the moment", "in the future", "past stay"].sample,
-    user_id: user.id,
-    flat_id: flat.id
+    user_id: rand(User.first.id..User.last.id),
+    flat_id: rand(Flat.first.id..Flat.last.id)
   )
   puts "booking request ##{i + 1}, #{booking_request.stay_status}, #{booking_request.direction}"
 
   review = Review.create!(
-    content: "random...",
+    content: ["good", "bad", "terrible", "amazing"].sample,
     rating: rand(5..10),
     booking_request_id: booking_request.id
   )
   puts "reviews ##{i + 1}, #{review.content} #{review.rating}"
 
   chat = Chat.create!(
-    flat_id: flat.id
+    flat_id: rand(Flat.first.id..Flat.last.id)
   )
   puts "chat ##{i + 1}, chating with user ##{chat.flat_id}"
 
   message = Message.create!(
-    content: "Message",
-    user_id: user.id,
+    content: ["Message", "something", "else"].sample,
+    user_id: rand(User.first.id..User.last.id),
     chat_id: chat.id
   )
   puts "message: ##{message.content}, from: user ##{message.user_id}, on: chat ##{message.chat_id}"
