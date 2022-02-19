575e6bebcc25261e2ab103b3ce332b467ff893f5
Camilo Zamora <cazmera.music@gmail.com>
2022-02-19 11:33:37 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get :dashboard, to: 'pages#dashboard'
+  resources :flats, only: %i[create show index] do
+    resources :booking_requests, only: %i[new create show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
