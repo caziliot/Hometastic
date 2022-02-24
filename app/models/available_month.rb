24dcda9c014fdac153d6cdc5a24c1deae9db6661
Camilo Zamora <cazmera.music@gmail.com>
2022-02-24 07:54:57 +0100

    feat: edited seed so it contains available months

--- app/models/available_month.rb
+++ app/models/available_month.rb
@@ -1,4 +1,6 @@
 class AvailableMonth < ApplicationRecord
   belongs_to :flat
   validates_date :month_year, after: -> { Date.current }, message: "date must be after today"
+  validates_uniqueness_of :month_year, scope: :flat_id
+  validates :month_year, presence: true
 end
