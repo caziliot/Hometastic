feba7e43564f115e57af90b47431e0e08250a0cb
Camilo Zamora <cazmera.music@gmail.com>
2022-02-24 08:47:08 +0100

    feat: flatpickr bundled yarn added and js added

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,16 @@
+import flatpickr from "flatpickr";
+
+const initFlatpickr = () => {
+  flatpickr(".datepicker", {
+    plugins: [
+      new monthSelectPlugin({
+        shorthand: true, //defaults to false
+        dateFormat: "m.y", //defaults to "F Y"
+        altFormat: "F Y", //defaults to "F Y"
+        theme: "dark" // defaults to "light"
+      })
+    ]
+  });
+}
+
+export { initFlatpickr };
--- db/migrate/20220224071319_add_taken_to_available_months.rb
+++ db/migrate/20220224071319_add_taken_to_available_months.rb
@@ -1,5 +1,5 @@
 class AddTakenToAvailableMonths < ActiveRecord::Migration[6.1]
   def change
-    add_column :available_months, :taken, :boolean
+    add_column :available_months, :taken, :boolean, default: false
   end
 end
