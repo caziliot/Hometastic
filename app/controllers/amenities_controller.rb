5e7a2408da1b6b821933a0f284e9bf3a1cbd0d8a
Lorenzo Fiorista <lorenzo.fiorista@gmail.com>
2022-02-26 16:02:01 +0100

    add new routes and controller

---
+++ app/controllers/amenities_controller.rb
@@ -0,0 +1,23 @@
+class AmenitiesController < ApplicationController
+  def new
+    @flat = Flat.find(params[:flat_id])
+    @amenity = Amenity.new
+  end
+
+  def create
+    @amenity = Amenity.new(amenity_params)
+    @flat = Flat.find(params[:flat_id])
+    @amenity.flat = @flat
+    if @amenity.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def amenity_params
+    params.require(:amenity).permit(:title, :icon_class)
+  end
+end
