5b297408b5167ad2dc96c873840f74823c71fd61
Camilo Zamora <cazmera.music@gmail.com>
2022-03-05 12:21:37 +0100

    new flat form with address control input

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,10 +1,29 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('flat_city');
+  const addressInput = document.getElementById('flat_address');
+  const cityInput = document.getElementById('flat_city')
+  const reconfigurableAddress = {
+    type: 'address', // Search only for addresses names
+    aroundLatLngViaIP: false // disable the extra search/boost around the source IP
+  };
+
   if (addressInput) {
-    places({ container: addressInput });
+    var addressPlace = places({
+      container: addressInput,
+      type: 'address',
+      templates: {
+        value: function (suggestion) {
+          return suggestion.name;
+        }
+      }
+    }).configure(reconfigurableAddress);
+
+    addressPlace.on('change', function(e) {
+      cityInput.value = e.suggestion.city
+    })
   }
+
 };
 
 export { initAutocomplete };
