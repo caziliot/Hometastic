6390fbdf777f39894aa9b34789e29ff5a1e34daa
Camilo Zamora <cazmera.music@gmail.com>
2022-03-10 18:34:10 +0100

    last changes

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -6,9 +6,11 @@ const initFlatpickr = () => {
   flatpickr(".datepicker", {
     plugins: [
       new monthSelectPlugin({
+        mode: "multiple",
+        inline: true,
         altInput: true,
         dateFormat: "d-m-Y", //defaults to "F Y"
-        altFormat: "F Y", //defaults to "F Y"
+        altFormat: "F", //defaults to "F Y"
         theme: "dark", // defaults to "light
         parseDate: true
       })
