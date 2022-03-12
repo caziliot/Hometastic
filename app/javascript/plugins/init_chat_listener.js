39cecbef39783c11f598ff94f25ef2e0fe45299b
Camilo Zamora <cazmera.music@gmail.com>
2022-03-12 10:19:16 +0100

    test javascript

---
+++ app/javascript/plugins/init_chat_listener.js
@@ -0,0 +1,11 @@
+
+const initAutocomplete = () => {
+  const display = document.getElementById("chat-display");
+  const linkout = document.querySelector(".linkout");
+  const linkin = document.querySelector(".linkin");
+
+  linkout.addEventListener('click', (event) => {
+    console.log(event);
+  });
+
+};
