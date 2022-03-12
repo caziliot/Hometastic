54d0ded77a15f3df90567e91dfcdc6111f172195
Marcy Hundschell Dela Cruz <marcy.delacruz@gmail.com>
2022-03-12 13:33:59 +0100

    no change

--- app/controllers/available_months_controller.rb
+++ app/controllers/available_months_controller.rb
@@ -1,6 +1,22 @@
 class AvailableMonthsController < ApplicationController
+  def new
+    @available_month = AvailableMonth.new
+    @flat = Flat.find(params[:flat_id])
+  end
+
   def create
-    @user = current_user
-    @flat.user = current_user
+    @available_month = AvailableMonth.new(months_params)
+    @available_month.flat = @flat
+    if @available_month.save
+      redirect_to flat_path(@flat)
+    else
+      render 'flat/show'
+    end
+  end
+
+ private
+
+  def months_params
+    params.require(:available_month).permit(:month_year, :free)
   end
 end
