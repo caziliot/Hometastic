13cd2a0b7912f86676b0e1b79e9d1150367ab9c2
Camilo Zamora <cazmera.music@gmail.com>
2022-03-12 14:39:10 +0100

    javascript for messages and chats works

--- app/javascript/plugins/init_chat_listener.js
+++ app/javascript/plugins/init_chat_listener.js
@@ -1,11 +1,28 @@
+import { initChatRoomCable } from "../channels/chat_room_channel";
 
-const initAutocomplete = () => {
+const init_chat_listener = () => {
   const display = document.getElementById("chat-display");
-  const linkout = document.querySelector(".linkout");
+  const linkouts = document.querySelectorAll(".linkout");
   const linkin = document.querySelector(".linkin");
 
-  linkout.addEventListener('click', (event) => {
+  linkouts.forEach(link => {
+    link.addEventListener('click', function handleClick(event) {
+      event.preventDefault();
+      const url = link.href;
+      fetch(url, { headers: { 'Accept': 'text/plain' } })
+        .then(response => response.text())
+        .then((data) => {
+          display.innerHTML = data;
+          initChatRoomCable();
+        })
+    });
+
+  });
+
+  addEventListener('click', (event) => {
     console.log(event);
   });
 
-};
+}
+
+export { init_chat_listener };
